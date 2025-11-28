92c65b36709c4064620c8d867e5a5aa45bb38556
Ari Maulana <m.ari.maulana93@gmail.com>
2025-11-28 16:20:07 +0700

    [PTL-0002][Todo List] - Create endpoint get todo list

--- app/api/sheet/route.ts
+++ app/api/sheet/route.ts
@@ -48,3 +48,39 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
+
+export async function GET() {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const sheets = google.sheets({
+      version: "v4",
+      auth,
+    });
+
+    const spreadsheetId = process.env.GOOGLE_SHEET_ID;
+
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range: "Sheet1!A1:E", // Sesuaikan jika kolom berbeda
+    });
+
+    const values = response.data.values || [];
+
+    return NextResponse.json(
+      {
+        message: "Data berhasil diambil",
+        data: values,
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
